d47cbdcf3d255f56e5e1a7de4a8e86c1d71918ea
aguoblue <aguogggg@outlook.com>
2025-03-31 16:28:55 +0800

    修改bash

--- scripts/bash/egoplanner.sh
+++ scripts/bash/egoplanner.sh
@@ -2,8 +2,8 @@
 
 
 # 启动 Docker 容器中的 VINS 节点
-docker exec -it ros2-ego bash -c "
-cd ~/ego_planner_ros_humble &&
+docker exec -it ros2-ego(容器名字，需要替换) bash -c "
+cd ~/data/prj/ego-planner &&
 source ./install/setup.bash &&
 ros2 launch ego_planner single_run_in_sim.launch.py
 "
--- scripts/bash/vinsfusion.sh
+++ scripts/bash/vinsfusion.sh
@@ -2,8 +2,8 @@
 
 
 # 启动 Docker 容器中的 VINS 节点
-docker exec -it ros2-vins bash -c "
-cd ~/vinsfusion &&
+docker exec -it ros2-vins(容器名字，需要替换) bash -c "
+cd ~/data/prj/vins-fusion/ros2_ws &&
 source ./install/setup.bash &&
 ros2 run vins vins_node ./src/vinsfusion/config/euroc/euroc_stereo_imu_config.yaml
 "
